2bb5fe496671ee014607ddb418302532eca75091
sungbaekheo <sungbaek.heo@gmail.com>
2022-09-01 09:23:10 +0900

    sql study

--- programmers/중복 제거하기.sql
+++ programmers/중복 제거하기.sql
@@ -1,4 +1,4 @@
 -- https://school.programmers.co.kr/learn/courses/30/lessons/59408
 -- 동물 보호소에 들어온 동물의 이름은 몇 개인지 조회하는 SQL 문
 SELECT COUNT(DISTINCT NAME) FROM ANIMAL_INS
-WHERE NAME != 'NULL';
+WHERE NAME is not NULL;
